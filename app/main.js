36c8f7226f800c24c12a25d2187f0ee757696267
jorcalle11 <jorcalle11@gmail.com>
2016-06-19 17:40:31 -0500

    guardar y eliminar ciudades en localStorage

--- app/main.js
+++ app/main.js
@@ -6,10 +6,14 @@
   const CODE_ASCII_ENTER = 13;
   const today = new Date();
   let $btnAddCity = $('[data-button="addCity"]');
+  let $btnRemoveAllCities = $('[data-button="removeCities"]')
   let $txtCity = $('[data-search="nameCity"]');
+  let cities = [];
 
   geolocation();
+  loadSavedCitiesInLocalStorage();
   $btnAddCity.on('click',addNewCity);
+  $btnRemoveAllCities.on('click',removeAllCities);
   // $txtCity.on('keypress', (e)=> {
   //   if (e.which == CODE_ASCII_ENTER) {
   //     addNewCity(e);
@@ -33,7 +37,15 @@
 
   function getCurrentCity (data) {
     $('.loader').hide();
-    renderHTML(data);
+    $('.notFound').hide();
+    let city = {};
+    city.name = data.name;
+    city.time = today.toLocaleTimeString('es-CO',{ hour12: true });
+    city.icon = `${BASE_IMG}${data.weather[0].icon}.png`;
+    city.currentTemp = (data.main.temp_min - celsius).toFixed(1);
+    city.tempMax = (data.main.temp_max - celsius).toFixed(1);
+    city.tempMin = (data.main.temp - celsius).toFixed(1);
+    renderHTML(city);
   }
 
   function activeTemplate (id) {
@@ -41,15 +53,15 @@
     return document.importNode(t.content, true);
   }
 
-  function renderHTML (data) {
+  function renderHTML (city) {
     let clone = activeTemplate('#template--city');
 
-    clone.querySelector('[data-city]').innerHTML = data.name;
-    clone.querySelector('[data-time]').innerHTML = today.toLocaleTimeString('es-CO',{ hour12: true });
-    clone.querySelector('[data-icon]').src = `${BASE_IMG}${data.weather[0].icon}.png`;
-    clone.querySelector('[data-temp="min"]').innerHTML= (data.main.temp_min - celsius).toFixed(1);
-    clone.querySelector('[data-temp="max"]').innerHTML= (data.main.temp_max - celsius).toFixed(1);
-    clone.querySelector('[data-temp="current"]').innerHTML= (data.main.temp - celsius).toFixed(1);
+    clone.querySelector('[data-city]').innerHTML = city.name;
+    clone.querySelector('[data-time]').innerHTML = city.time;
+    clone.querySelector('[data-icon]').src = city.icon;
+    clone.querySelector('[data-temp="min"]').innerHTML= city.tempMin;
+    clone.querySelector('[data-temp="max"]').innerHTML= city.tempMax;
+    clone.querySelector('[data-temp="current"]').innerHTML= city.currentTemp;
     $('.cards-group').append(clone);
   }
 
@@ -60,7 +72,43 @@
   }
 
   function getNewCity (data) {
-    console.log(data);
-    renderHTML(data);
+    let city = {};
+    city.name = data.name;
+    city.time = today.toLocaleTimeString('es-CO',{ hour12: true });
+    city.icon = `${BASE_IMG}${data.weather[0].icon}.png`;
+    city.currentTemp = (data.main.temp_min - celsius).toFixed(1);
+    city.tempMax = (data.main.temp_max - celsius).toFixed(1);
+    city.tempMin = (data.main.temp - celsius).toFixed(1);
+    renderHTML(city);
+    cities.push(city);
+    saveCityInLocalStorage(cities);
+  }
+
+  function saveCityInLocalStorage(city) {
+    localStorage.setItem('cities',JSON.stringify(city));
+  }
+
+  function loadSavedCitiesInLocalStorage() {
+    if (localStorage.cities) {
+      let citiesTemp = JSON.parse(localStorage.cities);
+      citiesTemp.map((city) => {
+        cities.push(city);
+        renderHTML(city);
+      });
+    }
+  }
+
+  function removeAllCities (e) {
+    e.preventDefault();
+    if (localStorage.cities) {
+      delete localStorage.cities;
+      cities = [];
+      let t = document.querySelector('.cards-group');
+      let articles = t.querySelectorAll('article');
+      for (var i = 0; i < articles.length; i++) {
+        articles[i].remove();
+      }
+      $('.notFound').show();
+    }
   }
 })();
